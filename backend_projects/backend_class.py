c484003b55efd1e17099287a6085f5dd246aa7cc
dilwod <mr.mems.ut@gmail.com>
2023-07-07 14:09:02 +0500

    Some adds

---
+++ backend_projects/backend_class.py
@@ -0,0 +1,25 @@
+class UserValidator:
+    def __init__(self):
+        with open("names.csv", "r") as f:
+            self.users = f.read().splitlines()
+
+    def check_name(self, name):
+        for user in self.users:
+            args = user.split(",")
+            if args[0] == name:
+                return True
+        return False
+
+    def check_email(self, email):
+        for user in self.users:
+            args = user.split(",")
+            if args[2] == email:
+                return True
+        return False
+
+    def check_password(self, password):
+        for user in self.users:
+            args = user.split(",")
+            if args[1] == password:
+                return True
+        return False
--- backend_projects/backend_first.py
+++ backend_projects/backend_first.py
@@ -1,5 +1,6 @@
 import csv
 import re
+from backend_class import UserValidator
 
 while True:
     reg = input('1. Login'
@@ -18,31 +19,7 @@ while True:
 if reg == '3':
     print('out')
 
-class UserValidator:
-    def __init__(self):
-        with open("names.csv", "r") as f:
-            self.users = f.read().splitlines()
 
-    def check_name(self, name):
-        for user in self.users:
-            args = user.split(",")
-            if args[0] == name:
-                return True
-        return False
-
-    def check_email(self, email):
-        for user in self.users:
-            args = user.split(",")
-            if args[2] == email:
-                return True
-        return False
-
-    def check_password(self, password):
-        for user in self.users:
-            args = user.split(",")
-            if args[1] == password:
-                return True
-        return False
 # def check_name(name):
 #     with open("names.csv","r") as f:
 #         users = f.read().splitlines()
